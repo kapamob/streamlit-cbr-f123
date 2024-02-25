529d141ecb4cad8d4c24c747f666c29f255d6aa4
kapamob <161014431+kapamob@users.noreply.github.com>
2024-02-25 13:12:38 +0000

    some extras hello.py

--- Hello.py
+++ Hello.py
@@ -34,6 +34,10 @@ def run():
         Machine Learning and Data Science projects.
         **👈 Select a demo from the sidebar** to see some examples
         of what Streamlit can do!
+
+        - Banks Capital - list of banks by capital value from the Bank of Russia website.
+        - Plotting Demo, Dataframe Demo - streamlit demos, I will delete and replace with other projects later)
+        
         ### Want to learn more?
         - Check out [streamlit.io](https://streamlit.io)
         - Jump into our [documentation](https://docs.streamlit.io)
