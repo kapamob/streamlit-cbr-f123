1f424c5efbda01d272e3e665f7860c9089b892e2
kapamob <161014431+kapamob@users.noreply.github.com>
2024-06-17 18:12:59 +0000

    16

--- pages/1_Banks_capital.py
+++ pages/1_Banks_capital.py
@@ -96,9 +96,9 @@ def cbr_f123():
         df = df.merge(df_names, how = 'left')
         df = df.sort_values(by = "C3", ascending = [False]).head(v_num)
         df.insert(0, "RANK", range(1, 1 + len(df)))
-        st.dataframe(data=df, hide_index=True)
+        st.dataframe(df, hide_index=True)
         #st.dataframe(data = df, column_order = ('RANK','REGN','NAME_B','C3'), column_config = {'RANK':'№', 'REGN':'Рег.номер', 'NAME_B':'Наименование банка', 'C3':'Значение капитала'}, hide_index = True)
-        st.dataframe(data=df, column_order=("RANK","REGN","NAME_B","C3"), column_config={"RANK":"№", "REGN":"Рег.номер", "NAME_B":"Наименование банка", "C3":"Значение капитала"}, hide_index=True)
+        st.dataframe(df, column_order=("RANK","REGN","NAME_B","C3"), column_config={"RANK":"№", "REGN":"Рег.номер", "NAME_B":"Наименование банка", "C3":"Значение капитала"}, hide_index=True)
     st.text("Источник данных: https://www.cbr.ru/banking_sector/otchetnost-kreditnykh-organizaciy/")
     st.text("Репозиторий: https://github.com/kapamob/streamlit-cbr-f123")
 
