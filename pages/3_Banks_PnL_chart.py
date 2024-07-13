c1f044246b8a050dcd1d3d3beeb22c489d46946a
kapamob <161014431+kapamob@users.noreply.github.com>
2024-07-13 17:55:29 +0300

    Update 3_Banks_PnL_chart.py

--- pages/3_Banks_PnL_chart.py
+++ pages/3_Banks_PnL_chart.py
@@ -223,17 +223,23 @@ WHERE year(capital_money.dt) = 2024 and month(capital_money.dt) = 4
             )
         ))
 
-    fig.update_layout(barmode='relative', margin=dict(l=20, r=20, t=20, b=20), paper_bgcolor="rgb(175, 225, 255)", autosize=False,
-    width=1525,
-    height=685,
-    yaxis = dict(
+    fig.update_layout(
+        barmode='relative', 
+        margin=dict(l=20, r=20, t=20, b=20), 
+        paper_bgcolor="rgb(175, 225, 255)", 
+        autosize=False,
+        width=1525,
+        height=685,                 
+    )
+
+    fig.update_yaxes(
         tickformat="%Y", # Формат даты для меток
         tickmode='array',
         tickvals=[datetime.strptime(x, '%Y-%m-%d') for x in filtered_df["dt"].unique()],
         ticktext=filtered_df["dt"].unique(),
         dtick='year'
-    ),                 
     )
+
     return fig
 
 
