1af611f4c3b035add3eaf437567a5194ea13fe0c
kapamob <161014431+kapamob@users.noreply.github.com>
2024-07-13 18:26:06 +0300

    Update 3_Banks_PnL_chart.py

--- pages/3_Banks_PnL_chart.py
+++ pages/3_Banks_PnL_chart.py
@@ -110,12 +110,11 @@ def render_chart1(engine, year='2024'):
         filtered_df = final_df[final_df['bank_name'] == name]
         fig.add_trace(go.Bar(
             y=filtered_df["year"],
-            x=filtered_df["sim_itogo"],
-            text=['{:.2}B'.format(x / 1000000000) for x in filtered_df["sim_itogo"]],
+            x=filtered_df["sim_itogo"]/ 1000000000,
+            #text=['{:.2}B'.format(x / 1000000000) for x in filtered_df["sim_itogo"]],
             name=name,
             orientation='h',
-            #hoverinfo = 'x+name+y',
-            hoverinfo = text,
+            hoverinfo = 'x+name+y',
             marker=dict(
                 color=color[name],
             )
